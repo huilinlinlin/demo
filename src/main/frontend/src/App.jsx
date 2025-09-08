7229743f0d63a6d0168b85bb4b3576ccda1008b5
lynn_liu <lynn_liu@gss.com.tw>
2025-09-08 16:57:36 +0800

    initial commit

---
+++ src/main/frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import "@progress/kendo-theme-default/dist/all.css";
+import { Button } from '@progress/kendo-react-buttons';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8081/api/hello')
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+
+  return (
+    <>
+    <div>
+      <h1>{message || 'Loading...'}</h1>
+    </div>
+      <div>
+      <h1>Hello KendoReact + Vite</h1>
+      <Button primary={true}>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
